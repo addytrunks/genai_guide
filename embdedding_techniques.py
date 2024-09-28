a4eb2630c907a193819d4d649db1c97f87994c09
Adhithya Srivatsan <addytrunks@gmail.com>
2024-09-28 15:58:23 +0530

    feat: hugging face embedding

--- embdedding_techniques.py
+++ embdedding_techniques.py
@@ -1,11 +1,19 @@
+import os
 from dotenv import load_dotenv 
 from langchain_openai import OpenAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 load_dotenv()
 
-openai_embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
-
 text = "What is the capital of the moon?"
-query_result = openai_embeddings.embed_query(text)
 
+openai_embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+query_result = openai_embeddings.embed_query(text)
+print(len(query_result))
+
+
+os.environ["HF_TOKEN"] = os.getenv("HF_TOKEN")
+huggingface_embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+query_result = huggingface_embeddings.embed_query(text)
+print(len(query_result))
 
